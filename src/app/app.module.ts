c6022e54f9fc01ca024086bc40daf26152b5f4eb
Prosenjit Manna <developer.prosenjit@gmail.com>
2017-07-30 01:20:32 +0530

    ui added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
