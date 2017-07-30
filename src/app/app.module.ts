e06af08a75f9111b5c00693dcd5d9310d1060d14
Prosenjit Manna <developer.prosenjit@gmail.com>
2017-07-30 11:19:55 +0530

    create form and search form added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -12,7 +13,8 @@ import { ListComponent } from './list/list.component';
     ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import 'rxjs/add/operator/debounceTime';
 
 @Component({
   selector: 'todo-header',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  todoCreateForm: FormGroup;
+  searchForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private _fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.todoCreateForm = this._fb.group({
+      title: ['', Validators.required]
+    });
+    this.searchForm = this._fb.group({
+      term: []
+    });
+
+    // auto search
+    this.searchForm.valueChanges
+      .debounceTime(400)
+      .subscribe(res => {
+      this.search();
+    });
+  }
+
+  add() {
+    console.log('New todo data: ', this.todoCreateForm.value);
+  }
+
+  search() {
+    console.log('search term:', this.searchForm.value);
   }
 
 }
